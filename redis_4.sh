3c20597b4398938ce4df1195a8ecc80aab0a9db6
sc0v0ne <sc0v1n001@gmail.com>
2024-03-19 19:00:18 -0300

    update

---
+++ redis_4.sh
@@ -0,0 +1,14 @@
+LPUSH "realese_movies" "O Hobbit"
+#127.0.0.1:6379> LPUSH "realese_movies" "O Hobbit"
+#(integer) 1
+LPUSH "realese_movies" "Barbie"
+#127.0.0.1:6379> LPUSH "realese_movies" "Barbie"
+#(integer) 2
+LPUSH "realese_movies" "Rick and Morty"
+#127.0.0.1:6379> LPUSH "realese_movies" "Rick and Morty"
+#(integer) 3
+LPUSH "realese_movies" "Blue Period"
+#127.0.0.1:6379> LPUSH "realese_movies" "Blue Period"
+#(integer) 4
+ 
+
---
+++ redis_5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+redis-server --daemonize yes
+
+redis-cli
+# Expected: 127.0.0.1:6379>
+
